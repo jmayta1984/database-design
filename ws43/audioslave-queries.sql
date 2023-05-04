9914cc37775e6cb62d308d3334e5df1810f0b5e1
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2023-05-04 07:49:30 -0500

    Create audioslave-queries.sql

---
+++ ws43/audioslave-queries.sql
@@ -0,0 +1,28 @@
+-- DDL: Definición de estructuras de objetos de base de datos
+-- create
+-- alter
+-- drop
+
+-- DML: Manipulación de datos
+-- select
+-- insert
+-- update
+-- delete
+
+-- Modificar la tabla artistas, agregando un campo que permita almacenar
+-- el país de nacimiento del artista
+
+alter table artistas
+add pais varchar(100)
+
+insert into artistas (codigo, nombre)
+values (1, 'Madonna')
+
+insert into artistas (codigo, nombre)
+values	(2, 'Britney Spears'),
+		(3, 'Bono')
+
+insert into artistas (codigo, nombre, pais)
+values (4, 'Bon Jovi', 'USA')
+
+select * from artistas
