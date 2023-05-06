6c428ca81f373f9bba44565e2b0f9ae061a5081f
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2023-05-06 12:20:43 -0500

    Create northwind-queries.sql

---
+++ ws43/northwind-queries.sql
@@ -0,0 +1,57 @@
+-- select: que columnas
+-- from: que tablas
+-- where: que condiciones
+-- Recuperar las filas de una tablas dada una condición
+
+
+-- La relación de todos los empleados
+select *
+from Employees
+
+-- Listar el nombre y apellido de los empleados
+select FirstName, LastName
+from Employees
+
+-- Listar el nombre y apellido de los empleados del país de USA
+select FirstName, LastName, Country
+from Employees
+where Country = 'USA'
+
+-- Listar los nombres de los productos cuyo precio unitario sea mayor 18 pero menor a 100,
+-- mostrando primero los productos de mayor precio
+
+select ProductName, UnitPrice
+from Products
+where UnitPrice > 18 and UnitPrice < 100
+order by UnitPrice desc
+
+-- Indicar los países de procedencia de los clientes
+select distinct Country
+from Customers
+
+-- Indicar los nombres de los clientes que no
+-- sean de los siguientes países de Francia, Brasil y México
+
+select CompanyName, Country
+from Customers
+where Country not in ('France', 'Brazil', 'Mexico')
+
+-- Indicar los nombre de los clientes que comiecen con Ma
+select CompanyName
+from Customers
+where CompanyName like 'ma%'
+
+-- Indicar los nombre de los clientes con que contenga market
+select CompanyName
+from Customers
+where CompanyName like '%market%'
+
+-- Indicar los nombres de clientes que comiencen con la letra L o la letra M
+select CompanyName
+from Customers
+where CompanyName like '[lm]%'
+
+-- Indicar la cantidad de clientes
+select count(CustomerID) from Customers
+
+-- Indicar la cantidad de países distintos de procedencia de los clientes
