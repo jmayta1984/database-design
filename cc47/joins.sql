d2940463070de634cb0ca1f082d3d77006987935
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2023-05-17 20:59:15 -0500

    Update joins.sql

--- cc47/joins.sql
+++ cc47/joins.sql
@@ -66,3 +66,49 @@ select ProductName, sum(Quantity)
 from [Order Details] OD
     join Products P on P.ProductID = OD.ProductID
 group by ProductName
+
+-- Encontrar los pedidos que debieron despacharse a una ciudad o código postal diferente de la ciudad
+-- o código postal del cliente que los solicitó. Para estos pedidos, mostrar el país, ciudad y código postal
+-- del destinatario, así como la cantidad total de pedidos por cada destino
+
+select ShipCountry, ShipCity, ShipPostalCode, count(*) Quantity
+from Orders O
+join Customers C on O.CustomerID = C.CustomerID
+where (ShipCity <> City) or (ShipPostalcode <> PostalCode)
+group by ShipCountry, ShipCity, ShipPostalCode
+
+
+-- Seleccionar todas las compañías de envío (código y nombre) que hayan efectuado algún despacho a
+-- México entre el primero de enero y el 28 de febrero de 2018.
+-- Formatos sugeridos a emplear para fechas:
+-- • Formatos numéricos de fecha (por ejemplo, '4/15/2018')
+-- • Formatos de cadenas sin separar (por ejemplo, '20181207')
+
+select ShipperID, CompanyName
+from Orders O
+    join Shippers S on O.ShipVia = S.ShipperID
+where (ShipCountry = 'Mexico') and (ShippedDate between '20180101' and '20180228')
+
+-- Mostrar el nombre de las compañias (empresa de envio) que tienen ordenes pendienes de envio
+
+
+select  CompanyName
+from Orders O
+    join Shippers S on O.ShipVia = S.ShipperID
+where ShippedDate is null
+
+-- Mostrar los nombres y apellidos de los empleados junto con los nombres y apellidos de sus respectivos
+-- jefes
+
+select E.EmployeeID, E.LastName, E.FirstName, E.ReportsTo, B.EmployeeID, B.LastName, B.FirstName
+from Employees E
+left join Employees B on E.ReportsTo = B.EmployeeID
+
+
+select Country, year(OrderDate) Year, sum((Quantity*UnitPrice)*(1-Discount)) Total
+from [Order Details] OD
+    join Orders O on OD.OrderID = O.OrderID
+    join Employees E on O.EmployeeID = E.EmployeeID
+group by  Country, year(OrderDate)
+order by Total desc
+
