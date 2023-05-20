c91fc42e1701a6122e84706c253ba978b5db6ce7
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2023-05-20 10:59:34 -0500

    Update joins.sql

--- si35/joins.sql
+++ si35/joins.sql
@@ -62,3 +62,38 @@ from Orders O
 where ShippedDate is null
 group by C.CustomerID, CompanyName
 order by C.CustomerID
+
+-- Encontrar los pedidos que debieron despacharse a una ciudad o código postal diferente de la ciudad
+-- o código postal del cliente que los solicitó. Para estos pedidos, mostrar el país, ciudad y código postal
+-- del destinatario, así como la cantidad total de pedidos por cada destino.
+
+select ShipCountry, ShipCity, ShipPostalCode, count(*) as Quantity
+from Orders as O
+    join Customers as C on O.CustomerID = C.CustomerID
+where (ShipCity <> City) or (ShipPostalCode <> PostalCode)
+group by ShipCountry, ShipCity, ShipPostalCode
+
+-- Seleccionar todas las compañías de envío (código y nombre) que hayan efectuado algún despacho a
+-- México entre el primero de enero y el 28 de febrero de 2018.
+-- Formatos sugeridos a emplear para fechas:
+-- • Formatos numéricos de fecha (por ejemplo, '15/4/2018')
+-- • Formatos de cadenas sin separar (por ejemplo, '20181207')
+
+select distinct ShipperID, CompanyName
+from Orders O
+    join Shippers S on ShipVia = ShipperID
+where (ShipCountry = 'Mexico') and (OrderDate between '20180101' and '20180228')
+
+-- Mostrar los nombres y apellidos de los empleados junto con los nombres y apellidos de sus respectivos
+-- jefes
+
+select  E.LastName, E.FirstName, E.ReportsTo, B.LastName, B.FirstName
+from Employees E
+     left join Employees B on E.ReportsTo = B.EmployeeID
+
+-- Mostrar el monto total de cada pedido
+
+select OrderID, sum ((Quantity*UnitPrice)*(1-Discount)) as Total
+from [Order Details]
+group by OrderID
+
