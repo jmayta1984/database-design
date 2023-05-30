f00334b08267c4984d286c34dc53798eabd3dfc4
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2023-05-29 20:55:06 -0500

    Update procedures.sql

--- cc47/procedures.sql
+++ cc47/procedures.sql
@@ -34,5 +34,64 @@ begin
 end
 
 declare @Q int
-exec usp_delete_category 2, @Q output
+exec usp_delete_category @CategoryID = 2, @Quantity = @Q output
 print(@q)
+
+-- Crear un procedimiento almacenado que imprima los nombres y las unidades en stock
+-- de cada producto
+create procedure usp_print_stock
+as
+begin
+    declare @ProductName nvarchar(40)
+    declare @UnitInStock smallint
+    declare products_cursor cursor for
+    select ProductName, UnitsInStock
+    from Products
+
+    open products_cursor
+
+    fetch products_cursor into @ProductName, @UnitInStock
+    while (@@fetch_status = 0)
+    begin
+        print(concat(@ProductName, ': ', @UnitInStock))
+        fetch products_cursor into @ProductName, @UnitInStock
+    end
+
+    close products_cursor
+    deallocate products_cursor
+end
+
+exec usp_print_stock
+
+-- Crear un procedimiento almacenado que imprima los cantidad de pedidos realizados para un producto (nombre)
+-- en un determinado año
+
+-- OrderID, OrderDate --> Orders O
+-- OrderID, ProductID --> [Order Details] OD
+-- ProductName, ProductID --> Products P
+
+create procedure usp_print_stock_product
+    @Year int,
+    @ProductName nvarchar(40)
+as
+begin
+    declare @Quantity int
+    set @Quantity = (select TotalOrders from (select ProductName, year(OrderDate) Year, count(O.OrderID) TotalOrders
+    from Orders O
+        join [Order Details] OD on O.OrderID = OD.OrderID
+        join Products P on OD.ProductID = P.ProductID
+    where year(OrderDate) = @Year and ProductName = @ProductName
+    group by ProductName, year(OrderDate)) Temporal)
+    print(@Quantity)
+end
+
+exec usp_print_stock_product @Year = 2018, @ProductName = 'Chai'
+
+
+create trigger tr_categories on Categories
+for insert
+    as
+    print(N'Se insertó un registro')
+
+exec usp_insert_category 'Demo' , 'Demo'
+
